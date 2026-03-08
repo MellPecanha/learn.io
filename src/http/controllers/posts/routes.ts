19e191756b2d0d0bb31a9bc19bd260daf5fa8031
inatandev <natan.ferreira21@hotmail.com>
2026-03-07 23:21:55 -0300

    feat(posts): add create post endpoint
    Introduce a new POST /posts endpoint to handle post creation. The endpoint validates request body using Zod schema and uses a dedicated use case. Update the Posts entity and interface to make author_id optional to reflect business requirements.

--- src/http/controllers/posts/routes.ts
+++ src/http/controllers/posts/routes.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from 'fastify'
 import { findAllPosts } from './find-all-posts'
+import { createPost } from './create-posts'
 
 export async function postsRoutes(app: FastifyInstance) {
   app.get('/posts', findAllPosts)
+  app.post('/posts', createPost)
 }
