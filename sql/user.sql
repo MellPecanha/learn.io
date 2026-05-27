54ad00fad6bd6e32d27963ec4fff0cd6b5b3f871
Mell Peçanha <mellpecanha@gmail.com>
2026-05-26 22:10:46 -0300

    fix: update env variables and user role handling for consistency

--- sql/user.sql
+++ sql/user.sql
@@ -6,5 +6,5 @@ PASSWORD VARCHAR(258) NOT NULL
 
 
 -- Adiciona o tipo enum e a coluna na tabela user
-CREATE TYPE user_role_enum AS ENUM ('professor', 'aluno');
-ALTER TABLE "user" ADD COLUMN role user_role_enum DEFAULT 'aluno';
+CREATE TYPE user_role_enum AS ENUM ('PROFESSOR', 'ALUNO');
+ALTER TABLE "user" ADD COLUMN role user_role_enum DEFAULT 'ALUNO';
